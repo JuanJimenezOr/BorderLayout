2d1bc3f8b96e6399e9a614c9f83b881c2f1b8992
EduardoFregoso <jeedfregosoco@ittepic.edu.mx>
2016-02-02 12:15:02 -0700

    Update BorderLayout.java

--- BorderLayout.java
+++ BorderLayout.java
@@ -1,3 +1,20 @@
-public class BorderLayout{
+//librerias que puso el profe en clase
+import javax.swing.JFrame;
+import javax.swing.JButton;
+public class BorderLayout//creo que aquí falta algo, en unos ejemplos habla de un extends
+{
+  
+    //tamaño de la ventana x,y, y longitudes
+    this.setBounds (10,40,350,280);
+    //creación de la variable border
+    BorderLayout bd= new BorderLayout ();
+    this.setLayout(bd);
+    
+    
+    public static void main(String ar[])
+	{
+		hola m = new  hola();
+		m.ventana();		
+	}
 
 }
