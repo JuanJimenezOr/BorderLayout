4e2e3ad925ac61862571a9d3da81971ec0fd56d3
kealmendezsa@hotmail.com <kealmendezsa@hotmail.com>
2016-02-02 16:59:58 -0700

    Botones agregados(Tiene detalles)

--- BorderLayout.java
+++ BorderLayout.java
@@ -1,20 +1,24 @@
 //librerias que puso el profe en clasess
 import javax.swing.JFrame;
 import javax.swing.JButton;
-public class BorderLayout//creo que aquí falta algo, en unos ejemplos habla de un extends
-{
+public class BorderLayout extends JFrame{ //Teniamos que extenderle el JFrame, por eso se importaba la libreria JFrame.
+
   
     //tamaño de la ventana x,y, y longitudes
     this.setBounds (10,40,350,280);
     //creación de la variable border
     BorderLayout bd= new BorderLayout ();
     this.setLayout(bd);
-    
-    
-    public static void main(String ar[])
-	{
-		hola m = new  hola();
-		m.ventana();		
-	}
+    //Creacion de los controles
+    JButton btnNor = JButton("Norte"); 
+    JButton btnSur = JButton("Sur"); 
+    JButton btnCen = JButton("Centro");
+    JButton btnWest = JButton("Oeste");
+    JButton btnEast = JButton("Este");
+  
+    public static void main(String ar[]){
+	hola m = new  hola();
+	m.ventana();		
+    }//main
 
-}
+}//class
